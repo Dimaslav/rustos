b0eaa49032331d66c0b74e1c658fbcda3218a883
Дмитрий <shutkin.diman22@gmail.com>
2026-09-24 14:49:25 +0300

    Delete kernel/target directory

--- kernel/target/debug/build/compiler_builtins/2a9f8a72341263f3/out/build_script_build.d
+++
@@ -1,6 +0,0 @@
-C:\Users\shutk\Desktop\soft\rustos\kernel\target\debug\build\compiler_builtins/2a9f8a72341263f3\out\build_script_build.d: C:\Users\shutk\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\src\rust\library\compiler-builtins\compiler-builtins\build.rs C:\Users\shutk\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\src\rust\library\compiler-builtins\compiler-builtins\..\libm\configure.rs
-
-C:\Users\shutk\Desktop\soft\rustos\kernel\target\debug\build\compiler_builtins/2a9f8a72341263f3\out\build_script_build.exe: C:\Users\shutk\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\src\rust\library\compiler-builtins\compiler-builtins\build.rs C:\Users\shutk\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\src\rust\library\compiler-builtins\compiler-builtins\..\libm\configure.rs
-
-C:\Users\shutk\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\src\rust\library\compiler-builtins\compiler-builtins\build.rs:
-C:\Users\shutk\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\src\rust\library\compiler-builtins\compiler-builtins\..\libm\configure.rs:
