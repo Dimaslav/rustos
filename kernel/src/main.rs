c6e68eaa9fbdeacd940bac813326d0924a90400e
Дмитрий <shutkin.diman22@gmail.com>
2026-09-24 14:48:45 +0300

    Add files via upload

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,11 +2,23 @@
 #![no_main]
 
 use bootloader_api::{entry_point, BootInfo};
+use core::panic::PanicInfo;
 
 entry_point!(kernel_main);
 
 fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
     loop {
-        unsafe { core::arch::asm!("hlt"); }
+        unsafe {
+            core::arch::asm!("hlt");
+        }
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        unsafe {
+            core::arch::asm!("hlt");
+        }
     }
 }
